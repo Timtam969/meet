fd71d8b9879005e8bcf9de53532cc8bfa15639dc
Timothy Losper <tim_losper@yahoo.com>
2022-11-25 14:51:06 +0100

    commit title

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import {
   ResponsiveContainer,
   Legend,
 } from 'recharts';
+import EventGenre from './EventGenre';
 
 
 class App extends Component {
@@ -29,11 +30,74 @@ class App extends Component {
     showWelcomeScreen: undefined
   }
 
+  // async componentDidMount() {
+  //   this.mounted = true;
+  //   const isLocal =
+  //     window.location.href.startsWith("http://127.0.0.1") ||
+  //     window.location.href.startsWith("http://localhost");
+  //   if (navigator.onLine && !isLocal) {
+  //     const accessToken = localStorage.getItem("access_token");
+  //     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+  //     const searchParams = new URLSearchParams(window.location.search);
+  //     const code = searchParams.get("code");
+  //     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+  //     if ((code || isTokenValid) && this.mounted)
+  //       getEvents().then((events) => {
+  //         if (this.mounted) {
+  //           this.setState({
+  //             events: events.slice(0, this.state.numberOfEvents),
+  //             locations: extractLocations(events),
+  //           });
+  //         }
+  //       });
+  //   } else {
+  //     getEvents().then((events) => {
+  //       if (this.mounted) {
+  //         this.setState({
+  //           showWelcomeScreen: false,
+  //           events: events.slice(0, this.state.numberOfEvents),
+  //           locations: extractLocations(events),
+  //         });
+  //       }
+  //     });
+  //   }
+  // }
+
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
+    if (!navigator.onLine) {
+      this.setState({
+        warningText:
+          "It looks like you're not connected to the internet. Data was loaded from the cache.",
+      });
+    } else {
+      this.setState({
+        warningText: '',
+      });
+    }
+  }
+
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
 
   updateEvents = (location, eventCount) => {
     const { numberOfEvents } = this.state;
     if (location === undefined) location = this.state.selectedLocation;
-
     getEvents().then((events) => {
       const locationEvents =
         location === "all"
@@ -58,51 +122,13 @@ class App extends Component {
     return data;
   };
 
-  async componentDidMount() {
-    this.mounted = true;
-    const isLocal =
-      window.location.href.startsWith("http://127.0.0.1") ||
-      window.location.href.startsWith("http://localhost");
-    if (navigator.onLine && !isLocal) {
-      const accessToken = localStorage.getItem("access_token");
-      const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-      const searchParams = new URLSearchParams(window.location.search);
-      const code = searchParams.get("code");
-      this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-      if ((code || isTokenValid) && this.mounted)
-        getEvents().then((events) => {
-          if (this.mounted) {
-            this.setState({
-              events: events.slice(0, this.state.numberOfEvents),
-              locations: extractLocations(events),
-            });
-          }
-        });
-    } else {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({
-            showWelcomeScreen: false,
-            events: events.slice(0, this.state.numberOfEvents),
-            locations: extractLocations(events),
-          });
-        }
-      });
-    }
-  }
-
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
 
   render() {
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />;
     return (
       <div className="App">
         <h1 className='header'>The Meet App</h1>
-        <div>
+        {/* <div>
           {!navigator.onLine && (
             <OfflineAlert
               className='alert-centered'
@@ -110,7 +136,8 @@ class App extends Component {
               {'You are currently offline. The list of events may not be up-to-date.'}
             />
           )}
-        </div>
+        </div> */}
+        <OfflineAlert text={this.state.warningText} />
         <div>
           <CitySearch locations={this.state.locations}
             updateEvents={this.updateEvents} />
@@ -123,7 +150,9 @@ class App extends Component {
         <br />
         <div>
           <div className='header3'>Events</div>
-          <ResponsiveContainer width={1000}>
+          <div className='data-vis-wrapper'>
+            <EventGenre events={this.state.events} />
+            {/* <ResponsiveContainer width={1000}> */}
             <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
               <CartesianGrid />
               <XAxis type="category" dataKey="city" name="city" />
@@ -136,7 +165,8 @@ class App extends Component {
               <Tooltip cursor={{ strokeDasharray: "3 3" }} />
               <Scatter data={this.getData()} fill="#8884d8" />
             </ScatterChart>
-          </ResponsiveContainer>
+            {/* </ResponsiveContainer> */}
+          </div>
           <EventList events={this.state.events} />
           <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
         </div>
